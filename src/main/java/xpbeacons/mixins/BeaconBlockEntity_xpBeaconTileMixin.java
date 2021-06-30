92e36379b10bf1e0ff80eb44312937ea18ad9e40
NikitaCartes <nikich98@yandex.ru>
2021-06-30 09:55:28 +0300

    Fix null predicate

--- src/main/java/xpbeacons/mixins/BeaconBlockEntity_xpBeaconTileMixin.java
+++ src/main/java/xpbeacons/mixins/BeaconBlockEntity_xpBeaconTileMixin.java
@@ -10,6 +10,8 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,42 +20,38 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xpbeacons.XpBeaconsCategorySettings;
 import xpbeacons.XpBeaconsSimpleSettings;
 
+import java.util.function.Predicate;
+
 @Mixin(BeaconBlockEntity.class)
 public abstract class BeaconBlockEntity_xpBeaconTileMixin extends BlockEntity {
-
-    @Shadow private StatusEffect primary;
-
-    @Shadow private int level;
-
-    @Shadow private StatusEffect secondary;
 
     public BeaconBlockEntity_xpBeaconTileMixin(BlockEntityType<?> type, BlockPos blockPos, BlockState blockState) {
         super(type, blockPos, blockState);
     }
 
     @Inject(method="applyPlayerEffects", at=@At("RETURN"))
-    private void applyXpBasedStatusEffects(CallbackInfo ci) {
+    private static void applyXpBasedStatusEffects(World world, BlockPos pos, int beaconLevel, @Nullable StatusEffect primaryEffect, @Nullable StatusEffect secondaryEffect, CallbackInfo ci) {
 
-        if (XpBeaconsSimpleSettings.xpBeacons && this.primary != null && !this.world.isClient) {
-            int r = (int)Math.pow(2, this.level + 3);
-            int x1 = this.pos.getX() - r, x2 = this.pos.getX() + r,
-                    z1 = this.pos.getZ() - r, z2 = this.pos.getZ() + r,
-                    y1 = Math.max(this.pos.getY() - r, 0), y2 = Math.min(this.pos.getY() + r, this.world.getHeight());
+        if (XpBeaconsSimpleSettings.xpBeacons && primaryEffect != null && !world.isClient) {
+            int r = (int)Math.pow(2, beaconLevel + 3);
+            int x1 = pos.getX() - r, x2 = pos.getX() + r,
+                    z1 = pos.getZ() - r, z2 = pos.getZ() + r,
+                    y1 = Math.max(pos.getY() - r, 0), y2 = Math.min(pos.getY() + r, world.getHeight());
             Box range = new Box(x1, y1, z1, x2, y2, z2);
 
-            double statusMultiplier = getEffectSpecificAmplificationMultiplier(this.primary);
+            double statusMultiplier = getEffectSpecificAmplificationMultiplier(primaryEffect, primaryEffect);
 
-            if (this.secondary == null) {
+            if (secondaryEffect == null) {
                 statusMultiplier /= 2; // Use the secondary to unlock FULL POWA
-            } else if (this.secondary.equals(StatusEffects.REGENERATION)) {
+            } else if (secondaryEffect.equals(StatusEffects.REGENERATION)) {
                 statusMultiplier /= 2;  // If secondary is regen apply xp-based regen
-                applyEffectToAllPlayers(this.secondary, range, getEffectSpecificAmplificationMultiplier(this.secondary));
+                applyEffectToAllPlayers(world, secondaryEffect, range, getEffectSpecificAmplificationMultiplier(primaryEffect, secondaryEffect));
             }
 
-            applyEffectToAllPlayers(this.primary, range, statusMultiplier);
+            applyEffectToAllPlayers(world, primaryEffect, range, statusMultiplier);
         }
     }
-    private double getEffectSpecificAmplificationMultiplier(StatusEffect se) {
+    private static double getEffectSpecificAmplificationMultiplier(@Nullable StatusEffect primaryEffect, StatusEffect se) {
         double statusMultiplier = 0.0;
         double[] multipliersInOrder = // UGGGGG JAVA WON"T DO SWITCHES ON OBJECTS
                 {
@@ -74,15 +72,15 @@ public abstract class BeaconBlockEntity_xpBeaconTileMixin extends BlockEntity {
                         StatusEffects.STRENGTH
                 };
         for (int i = 0; i<effectsInOrder.length; i++) {
-            if (effectsInOrder[i].equals(this.primary)) {
+            if (effectsInOrder[i].equals(primaryEffect)) {
                 statusMultiplier = multipliersInOrder[i];
                 break;
             }
         }
         return statusMultiplier;
     }
-    private void applyEffectToAllPlayers(StatusEffect se, Box range, double statusMultiplier) {
-        for (PlayerEntity player : this.world.getEntitiesByClass(PlayerEntity.class, range, null)) {
+    private static void applyEffectToAllPlayers(World world, StatusEffect se, Box range, double statusMultiplier) {
+        for (PlayerEntity player : world.getEntitiesByClass(PlayerEntity.class, range, x -> true)) {
             int amplifier = (int)(Math.min((int)((double)player.experienceLevel / XpBeaconsCategorySettings.xpBeaconsMax * 255), 255) * statusMultiplier);
             player.addStatusEffect(new StatusEffectInstance(se, 400, amplifier, true, true));
         }
